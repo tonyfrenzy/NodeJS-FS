ddd973776176bf738c328918080bb499cc56527a
tonyfrenzy <tonyfrenzy@gmail.com>
2021-04-20 11:29:45 +0100

    done

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const http = require('http')
 const fs = require('fs')
+const fetch = require("node-fetch")
 
-data = {
-	name:"tony",
-	track:"backend"
-}
-	fs.writeFile('posts.json', JSON.stringify(data), function() {
-		console.log('file created...')
-	})
+let dataDirectory = 'result'
+let filePath = fs.mkdir(__dirname + '/' + dataDirectory, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
 const server = http.createServer(function (req, res) {
+	fetch('http://jsonplaceholder.typicode.com/posts?_start=0&_limit=10')
+	  .then(response => response.json())
+	  .then(json => {
+	  	console.log(json)
+		let stringedData = JSON.stringify(json, null, 2)
+		fs.writeFileSync(dataDirectory + '/posts.json', stringedData)	  
+	  })
+
 	res.writeHead(200)
-	res.end('Server worked!')
+	res.end('Data fetched & saved successfully!')
 })
 
 server.listen(8080, function() {
-	console.log('File served!!!')
+	console.log('Server is up...')
 })
